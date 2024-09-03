e9eb040383d7f9197b7f70b526b9c2dc5bb7bc09
gopalprojectx100 <gopal.sasmal.29.3@mountblue.tech>
2024-09-03 11:54:59 +0530

    Add problem 2 solution

--- testSolution.js
+++ testSolution.js
@@ -1,8 +1,14 @@
 const persons = require("./person");
-const { getAllEmails } = require("./solution");
+const { getAllEmails, getHobbiesOfaSpecificAge } = require("./solution");
 
 //    Given the dataset of individuals, write a function that accesses and returns the email addresses of all individuals.
 console.log(getAllEmails(persons));
-console.log(getAllEmails([]));
-console.log(getAllEmails(undefined));
-console.log(getAllEmails(null));
+// console.log(getAllEmails([]));
+// console.log(getAllEmails(undefined));
+// console.log(getAllEmails(null));
+
+//    Implement a function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
+getHobbiesOfaSpecificAge(persons, 30);
+// getHobbiesOfaSpecificAge(persons, "30");
+// getHobbiesOfaSpecificAge(persons, null);
+// getHobbiesOfaSpecificAge([], 30);
